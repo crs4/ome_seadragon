f5bc485bb00e4f2ef70d8a52213a905de757043f
Luca Lianas <luca.lianas@crs4.it>
2015-10-21 15:58:03 +0200

    fetch images informations when getting TAGs

--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+import logging
+from distutils.util import strtobool
 try:
     import simplejson as json
 except ImportError:
@@ -12,6 +14,9 @@ from django.shortcuts import render
 
 def check_app(request):
     return HttpResponse("ome_seadragon working!")
+
+
+logger = logging.getLogger(__name__)
 
 
 def _get_images_by_tag(tag_id, conn):
@@ -29,11 +34,23 @@ def _get_images_by_tag(tag_id, conn):
 @login_required()
 def get_tags_list(request, conn=None, **kwargs):
     tags = list()
+    try:
+        fetch_images = strtobool(request.GET.get('fetch_imgs'))
+    except ValueError:
+        logger.warn('Unable to cast %s to boolean, forcing false by default', request.GET.get('fetch_imgs'))
+        fetch_images = False
+    except AttributeError:
+        fetch_images = False
+    logger.debug('"fetch_imgs" value %r', fetch_images)
     for tag in conn.getObjects("TagAnnotation"):
+        imgs_list = list()
+        if fetch_images:
+            imgs_list = _get_images_by_tag(tag.getId(), conn)
         tags.append({
             "id": tag.getId(),
             "value": tag.getValue(),
-            "description": tag.getDescription()
+            "description": tag.getDescription(),
+            "images": imgs_list
         })
     return HttpResponse(json.dumps(tags), content_type='application/json')
 
@@ -41,16 +58,28 @@ def get_tags_list(request, conn=None, **kwargs):
 @login_required()
 def find_tags(request, conn=None, **kwargs):
     search_pattern = request.GET.get('query')
+    try:
+        fetch_images = strtobool(request.GET.get('fetch_imgs'))
+    except ValueError:
+        logger.warn('Unable to cast %s to boolean, forcing false by default', request.GET.get('fetch_imgs'))
+        fetch_images = False
+    except AttributeError:
+        fetch_images = False
+    logger.debug('"fetch_imgs" value %r', fetch_images)
     qservices = conn.getQueryService()
     qparams = omero.sys.ParametersI()
     qparams.addString('search_pattern', '%%%s%%' % search_pattern)
     query = "SELECT t FROM TagAnnotation t WHERE t.description LIKE :search_pattern OR t.textValue LIKE :search_pattern"
     tags = list()
     for res in qservices.findAllByQuery(query, qparams):
+        imgs_list = list()
+        if fetch_images:
+            imgs_list = _get_images_by_tag(res.getId().val, conn)
         tags.append({
             "id": res.getId().val,
             "value": res.getTextValue().val,
-            "description": res.getDescription().val
+            "description": res.getDescription().val,
+            "images": imgs_list
         })
     return HttpResponse(json.dumps(tags), content_type='application/json')
 
