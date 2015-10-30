68606fb017ddd1a64a0e001f581cd0691ed0d0ec
Luca Lianas <luca.lianas@crs4.it>
2015-10-30 12:29:27 +0100

    first working versione of OpenSeadragon  + Python openslide integration

--- settings.py
+++ settings.py
@@ -8,4 +8,10 @@ CUSTOM_SETTINGS_MAPPINGS = {
     # <ome_home>/bin/config set omero.web.ome_seadragon.repository $(./bin/omero config get omero.data.dir)
     'omero.web.ome_seadragon.repository': ['IMGS_REPOSITORY', None, identity, None],
     'omero.web.ome_seadragon.images_folder': ['IMGS_FOLDER', 'ManagedRepository', identity, None],
+    # deepzoom properties
+    'omero.web.ome_seadragon.deepzoom.overlap': ['DEEPZOOM_OVERLAP', 1, identity, None],
+    'omero.web.ome_seadragon.deepzoom.format': ['DEEPZOOM_FORMAT', 'png', identity, None],
+    'omero.web.ome_seadragon.deepzoom.limit_bounds': ['DEEPZOOM_LIMIT_BOUNDS', True, identity, None],
+    'omero.web.ome_seadragon.deepzoom.jpeg_tile_compression': ['DEEPZOOM_JPEG_COMPRESSION', 75, identity, None],
+    'omero.web.ome_seadragon.deepzoom.tile_size': ['DEEPZOOM_TILE_SIZE', 256, identity, None]
 }
--- urls.py
+++ urls.py
@@ -6,6 +6,11 @@ urlpatterns = patterns(
     # TEST
     url(r'^test/$', views.check_app, name='ome_seadragon_test'),
     url(r'test/repository/$', views.check_repository, name='ome_seadragon_test_repository'),
+    url(r'test/repository/(?P<image_id>[0-9]+)/$', views.check_image_path,
+        name='ome_seadragon_test_image_path'),
+    # EXAMPLES
+    url(r'^examples/viewer/(?P<image_id>[0-9]+)/$', views.get_example_viewer,
+        name='ome_seadragon_test_viewer'),
     # OMERO TAGS
     url(r'^get/annotations/$', views.get_annotations,
         name='ome_seadragon_get_annotations'),
@@ -15,4 +20,9 @@ urlpatterns = patterns(
         name='ome_seadragon_find_tags'),
     url(r'^get/imgs_by_tag/(?P<tag_id>[0-9]+)/$', views.find_images_by_tag,
         name='ome_seadragon_find_images_by_tag'),
+    # DEEPZOOM
+    url(r'^deepzoom/get/(?P<image_id>[0-9]+).dzi$', views.get_image_dzi,
+        name='ome_seadragon_image_dzi_metadata'),
+    url(r'^deepzoom/get/(?P<image_id>[0-9]+)_files/(?P<level>[0-9]+)/(?P<column>[0-9]+)_(?P<row>[0-9]+).(?P<tile_format>[\w]+)$',
+        views.get_tile, name='ome_seadragon_get_tile'),
 )
