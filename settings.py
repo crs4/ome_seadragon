dee06af4b33727f6688bed1df0858feeae3163a0
Luca Lianas <luca.lianas@crs4.it>
2015-11-12 15:12:02 +0100

    fixed doc

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ def identity(value):
 CUSTOM_SETTINGS_MAPPINGS = {
     'omero.web.ome_seadragon.search.default_group': ['DEFAULT_SEARCH_GROUP', None, identity, None],
     # configure this value using OMERO.cli
-    # <ome_home>/bin/config set omero.web.ome_seadragon.repository $(./bin/omero config get omero.data.dir)
+    # <ome_home>/bin/omero config set omero.web.ome_seadragon.repository $(<ome_home>/bin/omero config get omero.data.dir)
     'omero.web.ome_seadragon.repository': ['IMGS_REPOSITORY', None, identity, None],
     'omero.web.ome_seadragon.images_folder': ['IMGS_FOLDER', 'ManagedRepository', identity, None],
     # deepzoom properties
