ecef4fda6c8a1c7702d819b9721a19b1fccd263f
Luca Lianas <luca.lianas@crs4.it>
2016-02-08 17:31:38 +0100

    interface for generic rendering engine

---
+++ slides_manager/rendering_engine_interface.py
@@ -0,0 +1,45 @@
+from abc import ABCMeta, abstractmethod
+import os
+import logging
+
+from ome_seadragon import settings
+
+
+class RenderingEngineInterface(object):
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, image_id, connection):
+        self.connection = connection
+        self.image_id = image_id
+        self.logger = logging.getLogger(__name__)
+
+    def _get_image_object(self):
+        return self.connection.getObject('Image', self.image_id)
+
+    def _get_image_path(self):
+        img = self._get_image_object()
+        if img is None:
+            return None
+        else:
+            return os.path.join(
+                settings.IMGS_REPOSITORY,
+                settings.IMGS_FOLDER,
+                img.getImportedImageFilePaths()['server_paths'][0]
+            )
+
+    @abstractmethod
+    def get_openseadragon_config(self):
+        pass
+
+    @abstractmethod
+    def get_dzi_description(self):
+        pass
+
+    @abstractmethod
+    def get_thumbnail(self, width, height):
+        pass
+
+    @abstractmethod
+    def get_tile(self, level, column, row):
+        pass
