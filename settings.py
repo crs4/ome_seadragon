fb5ee8632973c0029b00b587d754e2aec1bf5e52
Luca Lianas <luca.lianas@crs4.it>
2016-02-08 17:32:43 +0100

    switched to the new classes based rendering engine system

--- settings.py
+++ settings.py
@@ -21,6 +21,8 @@ CUSTOM_SETTINGS_MAPPINGS = {
     # <ome_home>/bin/omero config set omero.web.ome_seadragon.repository $(<ome_home>/bin/omero config get omero.data.dir)
     'omero.web.ome_seadragon.repository': ['IMGS_REPOSITORY', None, identity, None],
     'omero.web.ome_seadragon.images_folder': ['IMGS_FOLDER', 'ManagedRepository', identity, None],
+    # default rendering engine
+    'omero.web.ome_seadragon.rendering_engine': ['IMAGES_RENDERING_ENGINE', 'openslide', identity, None],
     # deepzoom properties
     'omero.web.ome_seadragon.deepzoom.overlap': ['DEEPZOOM_OVERLAP', 1, identity, None],
     'omero.web.ome_seadragon.deepzoom.format': ['DEEPZOOM_FORMAT', 'jpeg', identity, None],
