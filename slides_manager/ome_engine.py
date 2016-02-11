a347d8557c0cccabe1191c656f25b8f97dc0c3bd
Luca Lianas <luca.lianas@crs4.it>
2016-02-11 09:11:17 +0100

    working DZI adapter that uses OMERO rendering engine

---
+++ slides_manager/ome_engine.py
@@ -0,0 +1,152 @@
+import math
+from lxml import etree
+from PIL import Image
+from cStringIO import StringIO
+
+from rendering_engine_interface import RenderingEngineInterface
+from ome_seadragon import settings
+from ome_seadragon.images_cache import CacheDriverFactory
+
+
+class OmeEngine(RenderingEngineInterface):
+
+    def __init__(self, image_id, connection):
+        super(OmeEngine, self).__init__(image_id, connection)
+
+    def _get_dzi_max_level(self, img=None):
+        if img is None:
+            img = self._get_image_object()
+        x = img.getSizeX()
+        y = img.getSizeY()
+        return int(math.ceil(math.log(max(x, y), 2)))
+
+    def _get_dzi_scale_factor(self, level, img=None):
+        max_level = self._get_dzi_max_level(img)
+        if level > max_level:
+            raise ValueError('Level %d is higher than max DZI level for the image' % level)
+        return math.pow(0.5, max_level - level)
+
+    def _get_ome_scale_map(self, img=None, swap_keys=False):
+        if img is None:
+            img = self._get_image_object()
+        tmp_map = img.getZoomLevelScaling()
+        ome_scale_map = dict((len(tmp_map) - k - 1, v) for k, v in tmp_map.iteritems())
+        if not swap_keys:
+            return ome_scale_map
+        else:
+            return dict((v, k) for k, v in ome_scale_map.iteritems())
+
+    def _get_ome_scale_factor(self, level, img=None):
+        return self._get_ome_scale_map(img)[level]
+
+    def _get_best_downscale_level(self, level, img_obj=None):
+        ome_scale_map = self._get_ome_scale_map(img_obj, swap_keys=True)
+        dzi_scale_factor = self._get_dzi_scale_factor(level, img_obj)
+        for ome_scale_factor, ome_level in sorted(ome_scale_map.iteritems(), reverse=True):
+            if ome_scale_factor < dzi_scale_factor:
+                return ome_level + 1
+        return ome_level
+
+    def _get_scale_factor(self, original_x, original_y, ome_scale_factor, dzi_scale_factor):
+        ome_w = original_x * ome_scale_factor
+        ome_h = original_y * ome_scale_factor
+        ome_diag = math.sqrt(math.pow(ome_w, 2) + math.pow(ome_h, 2))
+        dzi_w = original_x * dzi_scale_factor
+        dzi_h = original_y * dzi_scale_factor
+        dzi_diag = math.sqrt(math.pow(dzi_w, 2) + math.pow(dzi_h, 2))
+        return ome_diag / dzi_diag
+
+    def _get_ome_tile(self, ome_img, ome_level, dzi_level, column, row):
+        scale_factor = self._get_scale_factor(ome_img.getSizeX(), ome_img.getSizeY(),
+                                              self._get_ome_scale_factor(ome_level, ome_img),
+                                              self._get_dzi_scale_factor(dzi_level, ome_img))
+        ome_tile_size = (settings.DEEPZOOM_TILE_SIZE + 2 * settings.DEEPZOOM_OVERLAP) * scale_factor
+        ome_x = row * (settings.DEEPZOOM_TILE_SIZE * scale_factor)
+        if ome_x != 0:
+            ome_x -= settings.DEEPZOOM_OVERLAP * scale_factor
+        ome_y = column * (settings.DEEPZOOM_TILE_SIZE * scale_factor)
+        if ome_y != 0:
+            ome_y -= settings.DEEPZOOM_OVERLAP * scale_factor
+        if ome_x == 0 or (ome_x + ome_tile_size >= ome_img.getSizeX()):
+            ome_tile_size_x = ome_tile_size - (settings.DEEPZOOM_OVERLAP * scale_factor)
+        else:
+            ome_tile_size_x = ome_tile_size
+        if ome_y == 0 or (ome_y + ome_tile_size >= ome_img.getSizeY()):
+            ome_tile_size_y = ome_tile_size - (settings.DEEPZOOM_OVERLAP * scale_factor)
+        else:
+            ome_tile_size_y = ome_tile_size
+        # right now, we don't handle Z and T levels
+        self.logger.debug('Getting tile with settings: X %s Y %s W %s H %s L %s', ome_x, ome_y,
+                          ome_tile_size_x, ome_tile_size_y, ome_level)
+        try:
+            tile_buffer = StringIO(ome_img.renderJpegRegion(0, 0, ome_x, ome_y, ome_tile_size_x,
+                                                            ome_tile_size_y, level=ome_level))
+            ome_tile = Image.open(tile_buffer)
+            tile_w, tile_h = ome_tile.size
+            return ome_tile.resize((int(round(tile_w / scale_factor)),
+                                    int(round(tile_h / scale_factor))),
+                                   Image.LINEAR)
+        except TypeError:
+            # return a white tile
+            return Image.new('RGB', (settings.DEEPZOOM_TILE_SIZE, settings.DEEPZOOM_TILE_SIZE), 'white')
+
+    # TODO: IMPLEMENT THIS!!!!
+    def get_openseadragon_config(self):
+        return {
+            'mpp': 0
+        }
+
+    def get_dzi_description(self):
+        img = self._get_image_object()
+        if img:
+            dzi_root = etree.Element(
+                'Image',
+                attrib={
+                    'Format': str(settings.DEEPZOOM_FORMAT),
+                    'Overlap': str(settings.DEEPZOOM_OVERLAP),
+                    'TileSize': str(settings.DEEPZOOM_TILE_SIZE)
+                },
+                nsmap={None: 'http://schemas.microsoft.com/deepzoom/2008'}
+            )
+            etree.SubElement(dzi_root, 'Size',
+                             attrib={'Height': str(img.getSizeY()), 'Width': str(img.getSizeX())})
+            return etree.tostring(dzi_root)
+        else:
+            return None
+
+    def get_thumbnail(self, width, height):
+        cache = CacheDriverFactory().get_cache()
+        thumbnail = cache.thumbnail_from_cache(self.image_id, width, height,
+                                               settings.DEEPZOOM_FORMAT)
+        if thumbnail is None:
+            self.logger.info('No thumbnail loaded from cache, building it')
+            ome_img = self._get_image_object()
+            if ome_img:
+                thumbnail_buffer = StringIO(ome_img.getThumbnail(size=(width, height)))
+                thumbnail = Image.open(thumbnail_buffer)
+                cache.thumbnail_to_cache(self.image_id, thumbnail, width, height,
+                                         settings.DEEPZOOM_FORMAT)
+        else:
+            self.logger.info('Thumbnail loaded from cache')
+        return thumbnail, settings.DEEPZOOM_FORMAT
+
+    def get_tile(self, level, column, row):
+        cache = CacheDriverFactory().get_cache()
+        cache_params = {
+            'image_id': self.image_id,
+            'level': level,
+            'column': column,
+            'row': row,
+            'tile_size': settings.DEEPZOOM_TILE_SIZE,
+            'image_format': settings.DEEPZOOM_FORMAT
+        }
+        if cache_params['image_format'].lower() == 'jpeg':
+            cache_params['image_quality'] = settings.DEEPZOOM_JPEG_QUALITY
+        tile = cache.tile_from_cache(**cache_params)
+        if tile is None:
+            ome_img = self._get_image_object()
+            ome_level = self._get_best_downscale_level(level, ome_img)
+            tile = self._get_ome_tile(ome_img, ome_level, level, row=column, column=row)
+            cache_params['image_obj'] = tile
+            cache.tile_to_cache(**cache_params)
+        return tile, settings.DEEPZOOM_FORMAT
